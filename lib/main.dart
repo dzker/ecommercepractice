0eedfacb97c95e38b194083ae5f4ec85a8cac759
HARITH DZIKRI BIN HASHIM <harithdzikri12@gmail.com>
2024-03-27 16:47:53 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:ecommerce/widgets/shoplistwidget.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ShopX',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ShopListWidget(),
+    );
+  }
+}
---
+++ lib/widgets/cart_list.dart
@@ -0,0 +1,142 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ecommerce/models/shopping_cart.dart';
+import 'package:ecommerce/models/item.dart';
+
+class CartListWidget extends StatefulWidget {
+  final ShoppingCart cart;
+  CartListWidget({super.key, required this.cart});
+  @override
+  State<StatefulWidget> createState() {
+    return _CartListWidgetState();
+  }
+}
+
+class _CartListWidgetState extends State<CartListWidget> {
+  static const platform = MethodChannel('camellabs.com/payment');
+  Future<void> _checkout() async {
+    await platform.invokeMethod('charge', widget.cart.toMap);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> items = [];
+    widget.cart.items.forEach((c) {
+      items.add(_CartListItemWidget(
+        item: c,
+      ));
+      items.add(Padding(
+        padding: EdgeInsets.only(top: 8.0),
+      ));
+    });
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('My Cart'),
+          actions: <Widget>[
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.white),
+              ),
+              onPressed: () => this._checkout(),
+              child: Text("Checkout"),
+            ),
+          ],
+        ),
+        body: Container(
+            decoration: BoxDecoration(color: Color(0xfff0eff4)),
+            child: Stack(
+              children: <Widget>[
+                ListView(
+                  padding: EdgeInsets.only(bottom: 64),
+                  children: items,
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  height: 64,
+                  child: _CartListSummaryFooterWidget(
+                    totalPrice: widget.cart.formattedTotalPrice,
+                  ),
+                )
+              ],
+            )));
+  }
+}
+
+class _CartListSummaryFooterWidget extends StatelessWidget {
+  final String totalPrice;
+
+  _CartListSummaryFooterWidget({required this.totalPrice});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+            color: Color(0XFFF4F4F4),
+            border: Border(top: BorderSide(color: Colors.grey, width: 0.5))),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Center(
+              child: Row(
+            children: <Widget>[
+              Text(
+                'Total',
+                textAlign: TextAlign.left,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              Expanded(
+                  child: Text(
+                this.totalPrice,
+                textAlign: TextAlign.right,
+                style: Theme.of(context).textTheme.displayMedium, //subhead
+              ))
+            ],
+          )),
+        ));
+  }
+}
+
+class _CartListItemWidget extends StatelessWidget {
+  final Item item;
+  const _CartListItemWidget({required this.item});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          border: Border(
+              top: BorderSide(color: Colors.grey, width: 0.5),
+              bottom: BorderSide(color: Colors.grey, width: 0.5))),
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        children: <Widget>[
+          Container(
+            height: 64,
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Image.network(item.imageUrl),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 8.0),
+          ),
+          Expanded(
+              child: Text(
+            item.name,
+            style: const TextStyle(fontSize: 75, fontWeight: FontWeight.bold),
+            // Theme.of(context).textTheme.titleLarge.apply(fontSizeFactor: 0.75),
+          )),
+          const Padding(
+            padding: EdgeInsets.only(right: 8.0),
+          ),
+          Text(
+            item.formattedPrice,
+            textAlign: TextAlign.right,
+            style: Theme.of(context).textTheme.displayMedium, //.subhead
+          )
+        ],
+      ),
+    );
+  }
+}
